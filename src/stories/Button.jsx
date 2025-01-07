8e4466ce3181b478ef1f9fa29038c86914c2f8e3
Damyana Stoyanova <damyana.stoyanova@corsearch.com>
2025-01-07 16:09:07 +0100

    restored

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from 'antd';
 
 import PropTypes from 'prop-types';
 
---
+++ src/stories/Story.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, Layout, Header } from 'antd';
+
+/** Primary UI component for user interaction */
+export const Story = ({ primary, backgroundColor, size, label }) => {
+  return (<>
+    <Layout>
+      <Header></Header>
+    <Button>test test</Button>
+    </Layout>
+    </>
+  );
+};
